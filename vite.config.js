30c26f931568d63a37fbc0a2a443ce42d67665b3
danangace <danangbahari89@gmail.com>
2022-11-27 11:41:21 +0700

    build config update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://danangputrab.site/',
   plugins: [vue()],
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
