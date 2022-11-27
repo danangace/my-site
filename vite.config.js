db5d730653b03c0eb422980ee38ce4317dfb0161
danangace <danangbahari89@gmail.com>
2022-11-27 13:25:58 +0700

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
-  server: {
-    port: 3000
-  }
+  plugins: [vue()]
 })
